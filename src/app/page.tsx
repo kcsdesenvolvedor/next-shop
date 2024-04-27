4d004649165b3378935a238b059424766490229e
kcsdesenvolvedor <kcs.desenvolvedor@gmail.com>
2024-04-26 23:38:54 -0300

    Listagem de produtos parte01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,24 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Prod 01
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Prod 01
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Prod 01
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Prod 01
+        </div>
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
+          Prod 01
+        </div>
+      </div>
       <h1>Hello Word</h1>
     </div>
   );
